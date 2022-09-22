f2588a30c75fb026702fdd45ae9465713c9d4fe5
Adam Rosas <adamrosas27@gmail.com>
2022-09-22 13:28:40 -0700

    Add comments to calculateMortgage method

--- Main.java
+++ Main.java
@@ -21,6 +21,7 @@ class Main {
     // public static calculateMortgage method returns double
     public static double calculateMortgage(int principal, float annualInterest, byte years) {
         // define constants 12 and 100
+        // find the number of payments
 
     }
 
