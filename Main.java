3b07b48316040f8438e2fc866e5381c6eba85cb9
Adam Rosas <adamrosas27@gmail.com>
2022-09-22 13:11:40 -0700

    Add comments for methods

--- Main.java
+++ Main.java
@@ -5,3 +5,11 @@
 
 // import java.text.NumberFormat
 // import java.util.Scanner
+
+// Main Class
+
+// Main Method
+
+// public static calculateMortgage method returns double
+
+// public static readNumber method returns double
